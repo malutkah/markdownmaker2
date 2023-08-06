f0b983f000ac21a7c818265624de871a3db2bae6
malutkah <alan.tressin@outlook.com>
2023-08-06 13:21:09 +0200

    shiet

--- make.go
+++ make.go
@@ -9,8 +9,7 @@ func CreateMarkdown(text string, fileName string) error {
 	data := []byte(text)
 	err := os.WriteFile(fileName+".md", data, 0644)
 	if err != nil {
-		err = fmt.Errorf("\nERROR: CreateMarkdown: %v\n\n", err)
-		return err
+		return fmt.Errorf("\nERROR: CreateMarkdown: %v\n\n", err)
 	}
 	
 	return nil
